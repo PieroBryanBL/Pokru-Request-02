49dc6308ac29327c18e4792f09947f1dd34433f4
Piero Bryan BL <pierobryanst1@gmail.com>
2023-03-30 09:09:23 -0500

    CSS File Created

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 
+import './public/styles.css';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()]
