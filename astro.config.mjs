fed4c328f74708ce1b8df3d187ee2bda3a8201e4
Piero Bryan BL <pierobryanst1@gmail.com>
2023-04-04 23:01:15 -0500

    New content

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,6 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 
-import './public/styles.css';
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()]
